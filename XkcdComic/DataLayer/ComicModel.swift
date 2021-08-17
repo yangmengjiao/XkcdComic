accfb5eaa43b16f40a5e8414ddaf56ac64750512
yangmengjiao <mengjiaoshun@gmail.com>
2021-08-17 17:32:48 -0500

    - added data layer

---
+++ XkcdComic/DataLayer/ComicModel.swift
@@ -0,0 +1,15 @@
+//
+//  ComicModel.swift
+//  XkcdComic
+//
+//  Created by mengjiao on 8/17/21.
+//
+
+import Foundation
+
+struct ComicModel: Hashable {
+    var title: String
+    var alt: String
+    var img: String?
+}
+
---
+++ XkcdComic/DataLayer/Data/ComicData.swift
@@ -0,0 +1,16 @@
+//
+//  ComicData.swift
+//  XkcdComic
+//
+//  Created by mengjiao on 8/17/21.
+//
+
+import Foundation
+
+struct ComicData: Codable {
+    var num: Int
+    var title: String
+    var alt: String
+    var img: String?
+}
+
