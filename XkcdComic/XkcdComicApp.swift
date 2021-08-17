eb3e8f86ab7383cd94790814e5e473e763fed073
yangmengjiao <mengjiaoshun@gmail.com>
2021-08-17 17:01:06 -0500

    Initial Commit

---
+++ XkcdComic/XkcdComicApp.swift
@@ -0,0 +1,17 @@
+//
+//  XkcdComicApp.swift
+//  XkcdComic
+//
+//  Created by mengjiao on 8/17/21.
+//
+
+import SwiftUI
+
+@main
+struct XkcdComicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
